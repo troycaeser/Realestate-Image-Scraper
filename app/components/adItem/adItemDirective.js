18f4a9b4d9d6837daaab3e5a53d00012b770435b
Troy Huang <troycaeser@dyn-118-139-2-248.its.monash.edu.au>
2015-12-10 23:56:26 +1100

    added remove functionality to adItemDirective

--- app/components/adItem/adItemDirective.js
+++ app/components/adItem/adItemDirective.js
@@ -5,19 +5,16 @@ var AdItemDirective = function(){
 		restrict: 'AE',
 		templateUrl: 'app/components/adItem/adItemDirective.html',
 		link: function(scope, elem, attrs){
-			elem.bind('click', function(){
-				elem.children().css('background-color', 'red');
-				scope.$apply(function(){
-					scope.color = "yellow";
-				});
-			});
-
 			elem.find('img').bind('mouseenter', function(){
 				elem.find('img').css('opacity', '0.5');
 			});
 
 			elem.find('img').bind('mouseleave', function(){
 				elem.find('img').css('opacity', '1');
+			});
+
+			elem.find('button').bind('click', function(){
+				elem.parent().remove();
 			})
 		}
 	}
