c6b38d9b7f4468478b37f43661570ab6c0d2ff00
Troy Huang <troycaeser@dyn-118-139-59-83.its.monash.edu.au>
2016-01-03 21:18:23 +1100

    css modified

--- app/app.module.js
+++ app/app.module.js
@@ -1,15 +1,15 @@
-var app = angular.module('app', ['angular-loading-bar', 'ui.router']);
+var app = angular.module('app', ['angular-loading-bar', 'ui.router', 'ui.bootstrap', 'ui.sortable']);
 
 //testing
 
 app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider){
-	$urlRouterProvider.otherwise('/adItem');
+    $urlRouterProvider.otherwise('/adItem');
 
-	$stateProvider
-		.state('adItem', {
-			url: '/adItem',
-			templateUrl : 'app/components/adItem/adItem.html',
-			controller: 'adItemController',
-			controllerAs: 'adItemCtrl'
-		});
+    $stateProvider
+        .state('adItem', {
+            url: '/adItem',
+            templateUrl : 'app/components/adItem/adItem.html',
+            controller: 'adItemController',
+            controllerAs: 'adItemCtrl'
+        });
 }]);
--- app/components/adItem/adItemDirective.js
+++ app/components/adItem/adItemDirective.js
@@ -1,48 +1,48 @@
 var app = angular.module('app');
 
 var AdItemDirective = function(adItemService){
-	return {
-		restrict: 'AE',
-		templateUrl: 'app/components/adItem/adItemDirective.html',
-		link: Controls,
-		scope:{
-			imgsrc: "@"
-		}
-	}
+    return {
+        restrict: 'AE',
+        templateUrl: 'app/components/adItem/adItemDirective.html',
+        link: Controls,
+        scope:{
+            imgsrc: "@"
+        }
+    }
 };
 
 var Controls = function(scope, elem, attrs){
-	//define controls
-	elem.find('canvas').bind('mouseenter', function(){
-		elem.find('canvas').css('opacity', '0.5');
-	});
+    //define controls
+    elem.find('canvas').bind('mouseenter', function(){
+        /*elem.find('canvas').css('opacity', '0.5');*/
+    });
 
-	elem.find('canvas').bind('mouseleave', function(){
-		elem.find('canvas').css('opacity', '1');
-	});
+    elem.find('canvas').bind('mouseleave', function(){
+        elem.find('canvas').css('opacity', '1');
+    });
 
-	elem.find('canvas').bind('mouseenter', function(){
-		//console.log(canvas.toDataURL("image/png"));
+    elem.find('canvas').bind('mouseenter', function(){
+        //console.log(canvas.toDataURL("image/png"));
         console.log('mouseentered the canvas dawg!');
-	});
+    });
 
     elem.find('button').bind('click', function(){
         elem.find('canvas').remove(); 
-        this.remove();
+        elem.remove();
     });
 
-	var canvas = elem.find('canvas')[0];
-	var context = canvas.getContext('2d');
+    var canvas = elem.find('canvas')[0];
+    var context = canvas.getContext('2d');
 
-	var imageObj = new Image();
-	// imageObj.setAttribute('crossOrigin', '*');
+    var imageObj = new Image();
+    // imageObj.setAttribute('crossOrigin', '*');
 
-	imageObj.onload = function(){
-		context.drawImage(imageObj, 0, 0);
-	}
+    imageObj.onload = function(){
+        context.drawImage(imageObj, 0, 0);
+    }
 
     imageObj.src = scope.imgsrc;
-	//imageObj.src = adItemService.Templates;
+    //imageObj.src = adItemService.Templates;
 
     console.log(adItemService.Templates());
        // .then(function(response){
