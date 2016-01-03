d616d9e0ed25d2402463c5e5cfdd579e8540013e
Troy Huang <troycaeser@dyn-118-139-59-83.its.monash.edu.au>
2016-01-03 16:09:34 +1100

    fixed backend indentation

--- api/index.php
+++ api/index.php
@@ -1,19 +1,19 @@
 <?php
-	require 'vendor/autoload.php';
+    require 'vendor/autoload.php';
 
-	$app = new \Slim\Slim(array(
-		'mode' => 'development'
-	));
+    $app = new \Slim\Slim(array(
+        'mode' => 'development'
+    ));
 
-	require 'routes/crawl.php';
+    require 'routes/crawl.php';
 
-	$app->get('/hello/:name', function($name){
-		echo "Hello " . $name;
-	});
+    $app->get('/hello/:name', function($name){
+        echo "Hello " . $name;
+    });
 
-	$app->get('/', function() use ($app){
-		echo 'Troy is the home for apiasdasdfasdff';
-	});
+    $app->get('/', function() use ($app){
+        echo 'Troy is the home for apiasdasdfasdff';
+    });
 
-	$app->run();
+$app->run();
 ?>
