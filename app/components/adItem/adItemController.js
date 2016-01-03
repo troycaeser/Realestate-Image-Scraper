305d74c7d8d7d20014c71c64afea8bfcbb6db631
Troy Huang <troycaeser@dyn-118-139-59-83.its.monash.edu.au>
2016-01-03 18:02:01 +1100

    added inputs

--- app/components/adItem/adItemController.js
+++ app/components/adItem/adItemController.js
@@ -1,21 +1,27 @@
 var app = angular.module('app');
 
 var AdItemController = function(adItemService){
-	// this.links = "this should be a bunch of links";
-	this.adItemService = adItemService;
+    // this.links = "this should be a bunch of links";
+    this.adItemService = adItemService;
 };
 
 AdItemController.prototype.display = function(url){
-	var _this = this;
-	_this.adItemService.Crawl(url)
-		.then(function(response){
-			_this.links = response.data;
-		});
+    var _this = this;
+    _this.adItemService.Crawl(url)
+        .then(function(response){
+            _this.links = response.data;
 
-    _this.adItemService.Templates()
+            //displays input fields
+            _this.show = true;
+            _this.bedRoom = "number of bedrooms";
+            _this.bathRoom = "number of bathrooms";
+            _this.carport = "number of carport";
+        });
+
+    /*_this.adItemService.Templates()
         .then(function(response){
             _this.banner = response.data;
-        });
+        });*/
 }
 
 app.controller('adItemController', ['adItemService', AdItemController]);
--- gulpfile.js
+++ gulpfile.js
@@ -8,44 +8,52 @@ var gulp  = require('gulp'),
     rimraf = require('gulp-rimraf'),
     watch = require('gulp-watch'),
     php = require('gulp-connect-php'),
+    sass = require('gulp-sass'),
     browserSync = require('browser-sync').create();
 
 var reload = browserSync.reload;
 
 var paths = {
-	app: 'app/**/*.js',
-	dist: 'dist/'
+    app: 'app/**/*.js',
+    dist: 'dist/'
 };
 
 gulp.task('php', function(){
-	php.server({
-		base: './',
-		port: 8888,
-		keepalive: false
-	});
+    php.server({
+        base: './',
+        port: 8888,
+        keepalive: false
+    });
+});
+
+//watches sass as well
+gulp.task('sass', function(){
+    gulp.src('app/assets/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist'));
 });
 
 //delete all files in dist
 gulp.task('rimraf', function(){
-	return gulp.src('dist/*.js', {read: false})
-		.pipe(rimraf());
+    return gulp.src('dist/*.js', {read: false})
+        .pipe(rimraf());
 });
 
-gulp.task('watch', ['minify', 'browserSync'], function(){
-	gulp.watch(['dist/app.js', 'api/**/*.php', 'app/**/*.html', 'app/assets/css/*.css', paths.app], ['minify']).on('change', browserSync.reload);
+gulp.task('watch', ['sass', 'minify', 'browserSync'], function(){
+    gulp.watch(['dist/app.js', 'api/**/*.php', 'app/**/*.html', 'app/assets/sass/*.scss', paths.app], ['minify', 'sass']).on('change', browserSync.reload);
 });
 
 //concatenate and minify
 gulp.task('minify', ['rimraf'], function() {
-	return gulp.src(paths.app)
-		.pipe(uglify())
-		.pipe(concat('app.min.js'))
-		.pipe(gulp.dest(paths.dist))
+    return gulp.src(paths.app)
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest(paths.dist))
 });
 
 //browserSync
 gulp.task('browserSync', ['php'], function(){
-	browserSync.init({
-		proxy: 'localhost:8888'
-	})
+    browserSync.init({
+        proxy: 'localhost:8888'
+    })
 });
