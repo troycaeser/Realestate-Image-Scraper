6a33e04fef81c71410fe73205b8012e98b8f0c53
Troy Huang <troycaeser@dyn-118-139-59-83.its.monash.edu.au>
2016-01-03 22:49:20 +1100

    added random files

---
+++ api/functions/makeTemplateDir.php
@@ -0,0 +1,22 @@
+<?php
+	function makeTemplateDir ($url, &$templateDir) {
+		$propertyInfo = array();
+		getHTML ($url, $propertyInfo);
+
+		$propertyDir = "{$_SERVER['DOCUMENT_ROOT']}/api/assets/agentAssets/";
+		//$propertyDir = "../assets/agentAssets/";
+		
+		$propertyDir .= $propertyInfo['agency_localDir'];
+		$propertyDir .= '/';
+
+		$templateDir = array (
+			'banner' => $propertyDir . "banner.png",
+			'bath' => $propertyDir . "bath.png",
+			'bed' => $propertyDir . "bed.png",
+			'bottom' => $propertyDir . "bottom.png",
+			'car' => $propertyDir . "car.png",
+			'font' => $propertyDir . "font.ttf",
+			'logo' => $propertyDir . "logo.png"
+		);
+	}
+?>
