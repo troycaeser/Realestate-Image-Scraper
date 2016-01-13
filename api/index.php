9570bb8104b0dc412db9f81fd9362b34001ab1de
Troy Huang <troycaeser@dyn-118-139-3-28.its.monash.edu.au>
2016-01-13 23:00:53 +1100

    checked that database is working

--- api/index.php
+++ api/index.php
@@ -15,6 +15,27 @@
 		echo 'You have reached the API home route for Linkubi MMS App';
 	});
 
+	$app->get('/agency', function(){
+		$dbh = getConnection();
+
+		$sql = "SELECT * FROM agency";
+		$stmt = $dbh->prepare($sql);
+		$stmt->execute();
+		$rows = $stmt->fetchALL(PDO::FETCH_OBJ);
+
+		echo json_encode($rows);
+	});
+
+	function getConnection(){
+		$hostname = 'localhost';
+		$username = 'root';
+		$password = 'root';
+		$dbname = 'linkubi';
+		$dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+
+		return $dbh;
+	}
+
 /*
 	include "{$_SERVER['DOCUMENT_ROOT']}/myApp/api/lib/getHTMLContents.php";
 	$app->get ('/testHTML', function() use ($app) {
